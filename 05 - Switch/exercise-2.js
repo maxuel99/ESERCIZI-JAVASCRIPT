574ae3afd77cee7431a78b01839442edd8a9bfcf
MassimoCaser <104093060+maxuel99@users.noreply.github.com>
2022-06-10 16:57:59 +0200

    Add files via upload

---
+++ 05 - Switch/exercise-2.js
@@ -0,0 +1,31 @@
+// ceo => 2200
+// manager => 1800
+// cto => 1800
+// developer => 1500
+// default => 1000
+function calculateSalary(role) {
+  switch (role) {
+    case 'ceo':
+      return (role + ' => 2200€');
+    case 'manager':
+      return (role + ' => 1800€');
+    case 'cto':
+      return (role + ' => 1800€');
+    case 'developer':
+      return (role + ' => 1500€');
+    default:
+      return ('default' + ' => 1000€');
+  } 
+}
+
+const ceoSalary = calculateSalary('ceo');
+const managerSalary = calculateSalary('manager');
+const ctoSalary = calculateSalary('cto');
+const developerSalary = calculateSalary('developer');
+const otherSalary = calculateSalary('other');
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
