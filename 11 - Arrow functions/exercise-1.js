83560b8192a4fbfa52ba26b3ca95fa94e7a8e0a6
MassimoCaser <104093060+maxuel99@users.noreply.github.com>
2022-06-12 21:01:27 +0200

    Add files via upload

---
+++ 11 - Arrow functions/exercise-1.js
@@ -0,0 +1,15 @@
+/*const sum = function (a, b) {
+  return a + b;
+}
+
+const log = function (value) {
+  console.log(value);
+}
+
+log(sum(2, 5));*/
+
+const sum = (a, b) => a + b;
+
+const log = (value) => console.log(value);
+
+log(sum(2, 5));
