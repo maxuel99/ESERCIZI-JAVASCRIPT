2ddd2509c58e334edb64dbc9e73c839021b2f393
MassimoCaser <104093060+maxuel99@users.noreply.github.com>
2022-06-16 02:10:10 +0200

    Update exercise-1.js

--- 27 - Optional chaining/exercise-1.js
+++ 27 - Optional chaining/exercise-1.js
@@ -1,13 +1,7 @@
 const person = {
   firstName: 'John',
-  lastName: 'Doe',
-  address: {
-    city: 'London',
-  },
-  fullName: function() {
-    return `${this.firstName} ${this.lastName}`;
-  }
+  lastName: 'Doe'
 };
 
-console.log(person.address.city); // Argh! Cannot read property 'city' of undefined
-console.log(person.fullName()); // Argh! Cannot read property 'fullName()' of undefined
+console.log(person.address?.city); // Argh! Cannot read property 'city' of undefined
+console.log(person.fullName?.()); // Argh! Cannot read property 'fullName()' of undefined
