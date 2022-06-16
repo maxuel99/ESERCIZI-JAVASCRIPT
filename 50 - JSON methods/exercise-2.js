227e15d0965f96f3717b052cd3fd4524df2a5583
MassimoCaser <104093060+maxuel99@users.noreply.github.com>
2022-06-16 22:44:23 +0200

    Add files via upload

---
+++ 50 - JSON methods/exercise-2.js
@@ -0,0 +1,22 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  static fromJson(json) {
+    json = JSON.parse(json);
+    json = new Person(1, "Mario", "Rossi", 25);
+    return json;
+  }
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+}
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+const developer = Person.fromJson(json);
+console.log(developer);
