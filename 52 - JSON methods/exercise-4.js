ab64aaceb16f027d4429c008270b6160392c55f4
MassimoCaser <104093060+maxuel99@users.noreply.github.com>
2022-06-16 15:24:05 +0200

    Add files via upload

---
+++ 52 - JSON methods/exercise-4.js
@@ -0,0 +1,15 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25,
+};
+
+const json = JSON.stringify(person, function(key, value) {
+  if (typeof value === 'string') {
+    return undefined;
+  }
+  return value
+});
+
+console.log(json); // Should return: { id: 1, age: 25 }
