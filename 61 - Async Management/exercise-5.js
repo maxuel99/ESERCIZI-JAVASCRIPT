4eb3ade3cc6c486d7b99050d5c809c1e553297a5
MassimoCaser <104093060+maxuel99@users.noreply.github.com>
2022-06-20 10:57:54 +0200

    Add files via upload

---
+++ 61 - Async Management/exercise-5.js
@@ -0,0 +1,53 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+const jobs = [
+  {
+    id: 1,
+    jobTitle: 'CEO'
+  },
+  {
+    id: 2,
+    jobTitle: 'Project Manager'
+  },
+  {
+    id: 3,
+    jobTitle: 'Developer'
+  }
+];
+
+// core here
+function fetchPersonById(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(persons.find(item => item.id === id)), 1000);
+  });
+}
+
+function fetchJobById(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(jobs.find(item => item.id === id)), 500);
+  });
+}
+
+Promise.race([fetchPersonById(1),fetchJobById(3)])
+.then(values => {
+  console.log(values);
+});
